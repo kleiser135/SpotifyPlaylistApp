ead6794ca11618b7d34c1e07829b4ebe83fd969b
Nicholas Kleiser <86692735+kleiser135@users.noreply.github.com>
2022-09-22 03:16:06 -0500

    Imported Components
    Imported components into app.js
    Created example search results

--- spotify-playlist-creator/src/App/App.js
+++ spotify-playlist-creator/src/App/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
+import SearchResults from '../SearchResults/SearchResults';
+import SearchBar from '../SearchBar/SearchBar';
+import PlayList from '../PlayList/PlayList';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'name',
+          artist: 'artist',
+          album: 'album',
+          id: 'id'}
+      ]
+    }
+  }
   render() {
     return (
       <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        {/*Add a SearchBar component*/}
+        <SearchBar/>
         <div className="App-playlist">
-          {/*Add a SearchResults component*/}
-          {/*Add a Playlist component*/}
+          <SearchResults searchResults={this.state.searchResults}/>
+          <PlayList/>
         </div>
       </div>
     </div>
