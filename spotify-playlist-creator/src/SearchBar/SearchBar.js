092d848ffc82e512f6a18981401e19e4a5fa6a07
Nicholas Kleiser <86692735+kleiser135@users.noreply.github.com>
2022-09-27 04:01:38 -0500

    Added searchbar functionality
    Allow search method in app.js to be passed to searchbar component. Allowed search bar to set its search term within its state.

--- spotify-playlist-creator/src/SearchBar/SearchBar.js
+++ spotify-playlist-creator/src/SearchBar/SearchBar.js
@@ -3,10 +3,24 @@ import './SearchBar.css';
 import '../TrackList/TrackList';
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    search(){
+        this.props.onSearch(this.state.term);
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
                 <button className="SearchButton">SEARCH</button>
             </div>
         );
