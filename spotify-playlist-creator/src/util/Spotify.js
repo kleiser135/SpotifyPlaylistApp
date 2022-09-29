8235ce18319f3e106f762f60481adafcace5c50a
Nicholas Kleiser <86692735+kleiser135@users.noreply.github.com>
2022-09-29 03:10:15 -0500

    Get spotify access token
    Created a method to allow the retrieval of a spotify access token for a user

--- spotify-playlist-creator/src/util/Spotify.js
+++ spotify-playlist-creator/src/util/Spotify.js
@@ -1,10 +1,28 @@
-import React from 'react';
+const redirectUri = 'http://localhost:3000';
+let accessToken;
 
-let userToken;
-
-class Spotify extends React.Component{
+const Spotify = {
     getAccessToken(){
+        if(accessToken){
+            return accessToken;
+        }
 
+        //check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch);
+            
+            //Clear parameters, grab new access token when expired.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
     }
 }
 
